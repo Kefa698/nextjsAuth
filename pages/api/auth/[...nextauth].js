343c2ad6addcf3404f624ce9c053f55f9671adc7
Kefa698 <kefaombongi07@gmail.com>
2022-05-13 15:18:56 +0300

    facebook
    Signed-off-by: Kefa698 <kefaombongi07@gmail.com>

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
 
 export default NextAuth({
 	// Configure one or more authentication providers
@@ -11,7 +12,11 @@ export default NextAuth({
 		}),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_ID,
-			clientSecret: process.env.GOOGLe_SECRET,
+			clientSecret: process.env.GOOGLE_SECRET,
+		}),
+		FacebookProvider({
+			clientId: process.env.FACEBOOK_ID,
+			clientSecret: process.env.FACEBOOK_SECRET,
 		}),
 		// ...add more providers here
 	],
